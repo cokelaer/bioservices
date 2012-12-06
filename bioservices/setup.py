68de2b7bba31cfbd641aeb9d0e5dc6c48cdb9213
Thomas Cokelaer <cokelaer@gmail.com>
2012-12-06 08:54:45 +0000

    add new directory and files

---
+++ bioservices/setup.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+__revision__ = "$Id$"
+import sys
+import os
+from setuptools import setup, find_packages
+import glob
+
+
+_MAJOR               = 0
+_MINOR               = 1
+_MICRO               = 0
+version              = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
+release              = '%d.%d' % (_MAJOR, _MINOR)
+
+metainfo = {
+    'authors': {
+        'Cokelaer':('Thomas Cokelaer','cokelaer@ebi.ac.uk'),
+        },
+    'version': version,
+    'license' : 'GPL',
+    'download_url' : ['http://pypi.python.org/pypi/bioservices'],
+    'url' : ['http://www.ebi.ac.uk/~cokelaer/rtools'],
+    'description':'utilities to ease manipulation of R packages using rpy2.' ,
+    'platforms' : ['Linux', 'Unix', 'MacOsX', 'Windows'],
+    'keywords' : ["bioservices", "signalling", "signalling pathways", "biological databases", "kegg"],
+    'classifiers' : [
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+          'Topic :: Scientific/Engineering :: Information Analysis',
+          'Topic :: Scientific/Engineering :: Mathematics',
+          'Topic :: Scientific/Engineering :: Physics']
+    }
+
+
+
+setup(
+    name             = 'bioservices',
+    version          = version,
+    maintainer       = metainfo['authors']['Cokelaer'][0],
+    maintainer_email = metainfo['authors']['Cokelaer'][1],
+    author           = metainfo['authors']['Cokelaer'][0],
+    author_email     = metainfo['authors']['Cokelaer'][1],
+    long_description = open("README.txt").read(),
+    keywords         = metainfo['keywords'],
+    description = metainfo['description'],
+    license          = metainfo['license'],
+    platforms        = metainfo['platforms'],
+    url              = metainfo['url'],      
+    download_url     = metainfo['download_url'],
+    classifiers      = metainfo['classifiers'],
+
+    # package installation
+    package_dir = {'':'src'},
+    packages = ['bioservices'],
+    #package_dir  = package_dir,
+    install_requires = ["easydev>=0.5.0"],
+    )
+
+
