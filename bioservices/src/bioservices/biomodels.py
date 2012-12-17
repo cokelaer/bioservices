931d88c1e8e32090d92d0db3f13943291d3df14e
Thomas Cokelaer <cokelaer@gmail.com>
2012-12-17 10:57:43 +0000

    finalise biomodels wrapping.

--- bioservices/src/bioservices/biomodels.py
+++ bioservices/src/bioservices/biomodels.py
@@ -17,6 +17,22 @@
 
 See http://www.ebi.ac.uk/biomodels-main/ for information about biomodels itself.
 
+
+Some keyword:
+
+======================= ============================================================
+identifier              Description/example
+======================= ============================================================
+ChEBIIds                identifiers of a ChEBI terms (e.g. CHEBI:4991)
+id                      model identifier (e.g. BIOMD0000000408 or MODEL1201250000)
+modelId                 model identifier (e.g. BIOMD0000000408 or MODEL1201250000)
+uniprotIds              Uniprot identifier (e.g., P41000)
+taxonomyId              Taxonomy identifier (e.g. 9606)
+GOId                    Gene Ontology identifier (e.g. GO:0006915)
+publicationIdOrText     publication identifier (PMID or DOI) or text
+======================= ============================================================
+                        which occurs in the publication's title or abstract
+
 """
 
 
@@ -187,13 +203,41 @@ a given model.
         """
         return self.serv.getModelSBMLById(Id)
 
-    @checkId
-    def getModelSBMLByChEBI(self, Id):
-        return self.serv.getModelSBMLByChEBI(Id)
+    def getModelsIdByChEBI(self, Id):
+        return self.serv.getModelsIdByChEBI(Id)
 
-    @checkId
-    def getModelSBMLByChEBIId(self, Id):
-        return self.serv.getModelSBMLByChEBIId(Id)
+    def getModelsIdByChEBIId(self, Id):
+        """Retrieves the identifiers of all the models which are annotated with
+        a given ChEBI term.
+
+        :param str Id: identifier of a ChEBI term (e.g. CHEBI:4978)
+
+        .. doctest::
+ 
+            >>> b = BioModel()
+            >>> b.getModelsIdByChEBIId('CHEBI:4978'')
+            ['BIOMD0000000404']
+
+        """
+        return self.serv.getModelsIdByChEBIId(Id)
+
+    def getSimpleModelsByChEBIIds(self, Ids):
+        """Retrieves the models which are annotated with the given ChEBI terms.
+
+        :param str Id: identifier of a ChEBI term (e.g. CHEBI:4978)
+ 
+        :return: list with all models annotated with the provided ChEBI identifiers, 
+            as a TreeMap (which uses ChEBI identifiers as keys) 
+
+        """
+        return self.serv.getSimpleModelsByChEBIIds(Ids)
+
+    def getSimpleModelsRelatedWithChEBI(self):
+        """Retrieves all the models which are annotated with ChEBI terms."""
+        return self.serv.getSimpleModelsRelatedWithChEBI()
+
+
+
 
     @checkId
     def getPublicationByModelId(self, Id):
@@ -206,7 +250,7 @@ a given model.
         return self.serv.getPublicationByModelId(Id)
 
     @checkId
-    def getSimpleModelByIds(self, Id):
+    def getSimpleModelsByIds(self, Id):
         """Retrieves the main information about given models.
 
         :param str Id: a valid model Id. See :attr:`modelsId` attribute.
@@ -217,8 +261,18 @@ a given model.
         return self.serv.getSimpleModelsByIds(Id)
 
 
-    def getModelsIdByPerson(self, person):
-        return self.serv.getModelsIdByPerson(person)
+    def getModelsIdByPerson(self, personName):
+        """Retrieves the identifiers of all models which have a given person as
+        author or encoder.
+
+
+        :param str personName: author's or encoder's name
+        :return: list of models identifiers
+
+        """
+
+
+        return self.serv.getModelsIdByPerson(personName)
 
     def getSimpleModelsByReactomeIds(self, reacID):
         """Retrieves all the models which are annotated with the given Reactome
@@ -270,8 +324,11 @@ records.
         return self.serv.getModelsIdByName(name)
 
     def getModelsIdByPublication(self, pubId):
-        """
+        """Retrieves the identifiers of all models related to one (or more) publication(s).
 
+
+        :param str pubId: publication identifier PMID or DOI or text which occurs in the publications.
+        
         ::
 
             b.getModelsIdByPublication('18308339')
@@ -288,9 +345,52 @@ records.
 
             >>> b.getModelsIdByGO('0001756')
             ['BIOMD0000000201', 'BIOMD0000000275']
+
+        .. seealso:::meth:`getModelsIdByGOId`
+
         """
         return self.serv.getModelsIdByGO(goId)
 
+    def getModelsIdByTaxonomy(self, text):
+        """Retrieves the models which are associated to the provided Taxonomy text.
+
+        :param str text: free text
+
+        """
+        return self.serv.getModelsIdByTaxonomy(text)
+
+    def getModelsIdByTaxonomyId(self, taxonomyId):
+        """Retrieves the models which are annotated with the given taxon.
+
+
+        :param str text: free text
+
+        Taxonomy identifier (e.g. 9606)
+
+        """
+        return self.serv.getModelsIdByTaxonomyId(taxonomyId)
+
+
+    def getSubModelSBML(self, modelId, elementsIDs):
+        """
+
+
+        :param modelId: identifier of the model from which the sub-model will be extracted
+        :param elementsIDs: identifiers of the selected elements. Currently only
+            supports identifiers from compartments, species, and reactions.
+        """
+        return self.serv.getSubModelSBML(modelId, elementsIDs)
+
+
+    def getModelsIdByGOId(self, GOId):
+        """Retrieves the models which are annotated with the given Gene Ontology term.
+
+        :param str GOId: Gene Ontology identifier (e.g. GO:0006915)
+
+        .. seealso:::meth:`getModelsIdByGO`
+
+        """
+        return self.serv.getModelsIdByGOId(GOId)
 
 
     def extra_getReactomeIds(self, start=0, end=1000):
@@ -342,17 +442,5 @@ records.
             if 'P%s' % i in res:
                 Ids.append('P%s' % i)
         return Ids
-
-
-todo = [
- u'getModelsIdByGOId',
- u'getModelsIdByPerson',
- u'getModelsIdByTaxonomy',
- u'getModelsIdByTaxonomyId',
- u'getModelsIdByUniprot',
- u'getSimpleModelsByChEBIIds',
- u'getSimpleModelsRelatedWithChEBI',
- u'getSubModelSBML',
- ]
 
 
