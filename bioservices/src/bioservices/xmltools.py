d3f0d6886d132c4697c8f2e4a7f7cce1d7d3006f
Thomas Cokelaer <cokelaer@gmail.com>
2013-01-07 21:25:26 +0000

    add new tools in xmltools.

--- bioservices/src/bioservices/xmltools.py
+++ bioservices/src/bioservices/xmltools.py
@@ -1,17 +1,47 @@
 """This module will include common tools to manipulate XML files"""
 import xml.etree.ElementTree as ET
-
-
+import BeautifulSoup
 
 
 class easyXML(object):
+    """class to ease the introspection of XML document.
 
+
+        >>> import nciblast
+        >>> n = nciblast.NCIBlast()
+        >>> res = n.parameters() # res is an instance of easyXML
+	# You can retreive XML from this instance of easyXML and print the content
+        # in a more human-readable way.
+        >>> print res
+        >>> res.soup.findAll('id') # a Beautifulsoup instance is available
+        >>> res.root # and the root using xml.etree.ElementTree
+
+    """
     def __init__(self, data):
+        """Constructor
+
+        :param data: a document in XML format
+
+        """
         self.data = data[:]
         self.root = ET.fromstring(data)
+        self._soup = None
+        self.prettify = self.soup.prettify
+        self.findAll = self.soup.findAll
 
     def getchildren(self):
+        """returns all children of the root XML document"""
         return self.root.getchildren()
+
+    def _get_soup(self):
+        if self._soup == None:
+            self._soup = BeautifulSoup.BeautifulSoup(self.data)
+        return self._soup
+    soup = property(_get_soup)
+
+    def __str__(self):
+        txt = self.soup.prettify()
+        return txt
 
 
 class easyXML_RheaSearch(easyXML):
@@ -46,11 +76,6 @@ class easyXML_RheaSearch(easyXML):
                 if element.tag == 'id':
                     ids.append(element.text)
         return ids
-
-
-
-
-
 
     def get_reactions_idprefix(self):
         reactions = self.get_reactions_elements()
