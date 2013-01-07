6b551d1ac43d730aa3b1243b3d17dd5e361ee7fa
Thomas Cokelaer <cokelaer@gmail.com>
2013-01-07 21:26:03 +0000

    add depencency on BeautifulSoup

--- bioservices/setup.py
+++ bioservices/setup.py
@@ -59,7 +59,7 @@ setup(
     package_dir = {'':'src'},
     packages = ['bioservices'],
     #package_dir  = package_dir,
-    install_requires = ["easydev>=0.5.0"],
+    install_requires = ["easydev>=0.5.0", "BeautilfulSoup"],
     )
 
 
