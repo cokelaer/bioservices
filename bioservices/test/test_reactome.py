505830f88d5e9c69d548a8107552bbbd8015d06f
Thomas Cokelaer <cokelaer@gmail.com>
2013-01-27 19:28:31 +0000

    add test

--- bioservices/test/test_reactome.py
+++ bioservices/test/test_reactome.py
@@ -1,9 +1,12 @@
 from bioservices.reactome import Reactome
+import unittest
+
 
 class test_reactome(Reactome):
     def __init__(self):
         super(test_reactome, self).__init__(verbose=False)
 
+    @unittest.skip
     def test_queryPathwaysForReferenceIdentifiers(self):
         self.queryPathwaysForReferenceIdentifiers(["Q9Y266", "P17480", "P20248"])
 
