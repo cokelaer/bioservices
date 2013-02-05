defb35e0bf182650a6f26f4c127970c2bccf0885
Thomas Cokelaer <cokelaer@gmail.com>
2013-02-05 17:10:10 +0000

    Improving the KEGG module by adding the entire parsing of KEGG entries in KeggParser class. + improve version of showPathways.. There are quite a few extra functions from weblink that will not be added for the time being.

--- bioservices/setup.py
+++ bioservices/setup.py
@@ -8,7 +8,7 @@ import glob
 
 _MAJOR               = 0
 _MINOR               = 9
-_MICRO               = 4
+_MICRO               = 5
 version              = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
 release              = '%d.%d' % (_MAJOR, _MINOR)
 
