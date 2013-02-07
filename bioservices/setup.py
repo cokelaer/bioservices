1869c45c0a989b19627f9aabedb44876e932bbd8
Thomas Cokelaer <cokelaer@gmail.com>
2013-02-07 13:07:09 +0000

    update metadata

--- bioservices/setup.py
+++ bioservices/setup.py
@@ -8,7 +8,7 @@ import glob
 
 _MAJOR               = 0
 _MINOR               = 9
-_MICRO               = 6
+_MICRO               = 7
 version              = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
 release              = '%d.%d' % (_MAJOR, _MINOR)
 
