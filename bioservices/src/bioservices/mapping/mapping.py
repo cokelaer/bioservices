c9a3df10c929d84bf0639a62206953060564cf3a
Thomas Cokelaer <cokelaer@gmail.com>
2013-11-18 22:59:18 +0000

    add mapping module.

---
+++ bioservices/src/bioservices/mapping/mapping.py
@@ -0,0 +1,33 @@
+from bioservices import UniProt
+
+
+
+
+
+
+
+
+
+
+
+class Mapping(object):
+    """
+
+
+        Could use unichem, uniprot, kegg, chembldb
+
+
+
+    """
+
+
+    def __init__(self):
+        self._uniprot = UniProt()
+        self._mapping['uniprot'] = self._uniprot._mapping
+        self.databases = {}
+
+    def map(self):
+        pass
+
+    def search(self):
+        pass
