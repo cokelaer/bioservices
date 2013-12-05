98e30d6d4f1f5289bcc60d1d7c3ed7f87da76719
Thomas Cokelaer <cokelaer@gmail.com>
2013-12-05 19:11:50 +0000

    add test for the muscle module.wq

---
+++ bioservices/test/test_muscle.py
@@ -0,0 +1,22 @@
+from bioservices.dev.muscle import MUSCLE
+from bioservices import uniprot
+
+
+def test_muscle():
+    m = MUSCLE(verbose=False)
+    m.parameters
+    m.parametersDetails("format")
+
+
+    u = uniprot.UniProt(verbose=False)
+    f1 = u.searchUniProtId("P18812",format="fasta")
+    f2 = u.searchUniProtId("P18813",format="fasta")
+
+
+    jobid = m.run(format="fasta", sequence=f1+f2, email="cokelaer@ebi.ac.uk")
+    m.getStatus(jobid)
+    m.wait(jobid)
+
+    m.getResultTypes(jobid)
+    m.getResult(jobid, 'phylotree')
+
