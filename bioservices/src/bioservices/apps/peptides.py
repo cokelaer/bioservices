e317dd2df56f32537dadd95c12b63cdf06082a79
Thomas Cokelaer <cokelaer@gmail.com>
2013-12-05 17:28:41 +0000

    add verbose option in peptides

--- bioservices/src/bioservices/apps/peptides.py
+++ bioservices/src/bioservices/apps/peptides.py
@@ -7,13 +7,15 @@ class Peptides(object):
     ::
 
         >>> p = Peptides()
+        >>> p.get_fasta("Q8IYB3")
         >>> p.get_phosphosite_position("Q8IYB3", "VPKPEPIPEPKEPSPE")
         189
 
-    """
-    def __init__(self):
 
-        self.u = UniProt()
+    """
+    def __init__(self, verbose=False):
+
+        self.u = UniProt(verbose=verbose)
 
     def get_fasta(self, uniprot_name):
         seq = self.u.get_fasta(uniprot_name)
@@ -25,5 +27,7 @@ class Peptides(object):
             return fasta.index(peptide)
         else:
             return -1
+
+
 
 
