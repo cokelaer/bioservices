d9e982e97ce6e07ec77398f0a71a1afcd5c7ecd1
Thomas Cokelaer <cokelaer@gmail.com>
2013-12-06 15:26:17 +0000

    rename function in peptides module related to fasta files

--- bioservices/src/bioservices/apps/peptides.py
+++ bioservices/src/bioservices/apps/peptides.py
@@ -7,27 +7,33 @@ class Peptides(object):
     ::
 
         >>> p = Peptides()
-        >>> p.get_fasta("Q8IYB3")
+        >>> p.get_fasta_sequence("Q8IYB3")
         >>> p.get_phosphosite_position("Q8IYB3", "VPKPEPIPEPKEPSPE")
         189
 
+
+    Sometimes, peptides are provided with a pattern indicating the phospho site.
+    e.g., ::
+
+        >>>
 
     """
     def __init__(self, verbose=False):
 
         self.u = UniProt(verbose=verbose)
 
-    def get_fasta(self, uniprot_name):
-        seq = self.u.get_fasta(uniprot_name)
+    def get_fasta_sequence(self, uniprot_name):
+        seq = self.u.get_fasta_sequence(uniprot_name)
         return seq
 
     def get_phosphosite_position(self, uniprot_name, peptide):
-        fasta = self.get_fasta(uniprot_name)
+        fasta = self.get_fasta_sequence(uniprot_name)
         if peptide in fasta:
             return fasta.index(peptide)
         else:
             return -1
 
+    
 
 
 
