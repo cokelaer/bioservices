be0e10c44c915600cf228c331b68049c2f0c6eb4
Thomas Cokelaer <cokelaer@gmail.com>
2014-02-17 17:58:53 +0000

    return a list of positions. Can be duplicates indeed. besides, added a dictionary to buffer requests to uniprot.

--- bioservices/src/bioservices/apps/peptides.py
+++ bioservices/src/bioservices/apps/peptides.py
@@ -1,14 +1,14 @@
-
 from bioservices import UniProt
+import re
 
 class Peptides(object):
     """
-    
+
     ::
 
         >>> p = Peptides()
         >>> p.get_fasta_sequence("Q8IYB3")
-        >>> p.get_phosphosite_position("Q8IYB3", "VPKPEPIPEPKEPSPE")
+        >>> p.get_peptide_position("Q8IYB3", "VPKPEPIPEPKEPSPE")
         189
 
 
@@ -19,21 +19,23 @@ class Peptides(object):
 
     """
     def __init__(self, verbose=False):
-
         self.u = UniProt(verbose=verbose)
+        self.sequences = {}
 
     def get_fasta_sequence(self, uniprot_name):
         seq = self.u.get_fasta_sequence(uniprot_name)
         return seq
 
     def get_phosphosite_position(self, uniprot_name, peptide):
-        fasta = self.get_fasta_sequence(uniprot_name)
-        if peptide in fasta:
-            return fasta.index(peptide)
+        if uniprot_name not in self.sequences.keys():
+            seq = self.get_fasta_sequence(uniprot_name)
+            self.sequences[uniprot_name] = seq[:]
         else:
-            return -1
+            seq = self.sequences[uniprot_name][:]
+        positions = [x.start() for x in re.finditer("PQS", seq)]
+        return positions
 
-    
+
 
 
 
