b30ef1f585c53aad2f00cca6c87dbe8f47428763
cokelaer <cokelaer@gmail.com>
2015-01-07 14:39:32 +0000

    improve test robustness

--- test/test_psicquic.py
+++ test/test_psicquic.py
@@ -1,4 +1,4 @@
-from bioservices import PSICQUIC
+from bioservices import PSICQUIC, psicquic
 from nose.plugins.attrib import attr
 
 
@@ -22,6 +22,8 @@ class test_psicquic(object):
         assert N1 == N2
 
     def test_query(self):
+        if 'intact' not in self.s.activeDBs or 'matrixdb' not in self.s.activeDBs:
+            return
 
         self.s.query("intact", "brca2", "tab27")
         self.s.query("intact", "zap70", "xml25")
@@ -32,11 +34,11 @@ class test_psicquic(object):
         except:
             assert True
 
-
         self.s.query("matrixdb", "*", "xml25", firstResult=10, maxResults=10)
 
         # accessing the string DB
-        self.s.query("string", "species:10090", firstResult=0, maxResults=100, output="tab25")
+        if 'string' in self.s.activeDBs:
+            self.s.query("string", "species:10090", firstResult=0, maxResults=100, output="tab25")
 
 @attr('fixme')
 def test_appsPPI():
