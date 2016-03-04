de1f8fa6fa133c4be4dd378e203cb57ada0ad6a6
Thomas Cokelaer <cokelaer@gmail.com>
2016-03-04 11:54:17 +0100

    increment version [ci skip]

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import glob
 
 _MAJOR               = 1
 _MINOR               = 4
-_MICRO               = 6
+_MICRO               = 7
 version              = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
 release              = '%d.%d' % (_MAJOR, _MINOR)
 
