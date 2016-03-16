6f7df964dfe4d4a55e0403ef83b20e199a0e5188
Thomas Cokelaer <cokelaer@gmail.com>
2016-03-16 13:51:06 +0100

    Fixing omnipath test

--- test/test_omnipath.py
+++ test/test_omnipath.py
@@ -1,5 +1,5 @@
 from bioservices import OmniPath
-
+from nose.plugins.attrib import attr
 
 
 o = OmniPath(cache=True)
@@ -16,12 +16,33 @@ def test_net():
 def test_inter():
     o.get_interactions()
     o.get_interactions("P00533")
+    o.get_interactions(["P00533"])
     o.get_interactions("P00533,O15177,Q96FE5", frmt='json')
     o.get_interactions("P00533", frmt='json', fields=['sources', 'references'])
+
+    try:
+        o.get_interactions(00553)
+        assert False
+    except:
+        assert True
 
 def test_ptms():
     o.get_ptms()
     o.get_ptms("P00533")
+    o.get_ptms(["P00533"])
     o.get_ptms("P00533", frmt='json')
     o.get_ptms("P00533", frmt='json', fields=['sources', 'references'])
     
+    try:
+        o.get_ptms(00553)
+        assert False
+    except:
+        assert True
+
+def test_get_resources():
+    res = o.get_resources()
+
+@attr('skip_travis')
+def test_info():
+    o.get_info()
+
