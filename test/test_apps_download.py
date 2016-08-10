e9e855ed11afc48f1703e18cedd3c778f119bdff
Thomas Cokelaer <cokelaer@gmail.com>
2016-08-10 10:25:42 +0200

    add new test

---
+++ test/test_apps_download.py
@@ -0,0 +1,26 @@
+from bioservices.apps import download_fasta as df
+
+
+
+
+
+def test_ena():
+
+    from easydev import TempFile
+    with TempFile() as f1:
+        df.download_fasta("FN433596.1", output_filename=f1.name, method="ENA")
+
+    with TempFile() as f2:
+        df.download_fasta("FN433596.1", output_filename=f2.name, method="EUtils")
+
+    with TempFile() as f3:
+        from bioservices import ENA
+        ena = ENA()
+        df.download_fasta("FN433596.1", output_filename=f2.name, method="EUtils",
+            service=ena)
+
+    try:
+        df.download_fasta("FN433596.1", method="dummy")
+        assert False
+    except:
+        assert True
