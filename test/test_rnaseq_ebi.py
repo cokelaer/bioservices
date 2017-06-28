6872b50c42d2a55745ace2e54e1d4ec998ed549e
Thomas Cokelaer <cokelaer@gmail.com>
2017-06-28 11:35:05 +0200

    fix rnaseq tests

--- test/test_rnaseq_ebi.py
+++ test/test_rnaseq_ebi.py
@@ -1,47 +1,50 @@
 from bioservices import RNASEQ_EBI
+import pytest
+
+@pytest.fixture
+def rnaseq():
+    r = RNASEQ_EBI(cache=False)
+    assert 'homo_sapiens' in r.organisms
+    return r
 
 
-r = RNASEQ_EBI(cache=False)
-assert 'homo_sapiens' in r.organisms
+def test1(rnaseq):
+    rnaseq.get_run_by_organism("homo_sapiens", "tsv")
+    rnaseq.get_run_by_organism("homo_sapiens", "json")
+    rnaseq.get_run_by_organism("homo_sapiens",condition="central nervous system")
 
 
-def test1():
-    r.get_run_by_organism("homo_sapiens", "tsv")
-    r.get_run_by_organism("homo_sapiens", "json")
-    r.get_run_by_organism("homo_sapiens",condition="central nervous system")
+def test2(rnaseq):
+    rnaseq.get_run_by_study("SRP033494", mapping_quality=90, frmt='tsv')
 
 
-def test2():
-    r.get_run_by_study("SRP033494", mapping_quality=90, frmt='tsv')
-
-
-def test3():
-    res = r.get_study("SRP033494", "tsv")
-    res = r.get_study("SRP033494", frmt="json")
+def test3(rnaseq):
+    res = rnaseq.get_study("SRP033494", "tsv")
+    res = rnaseq.get_study("SRP033494", frmt="json")
     assert res[0]['STUDY_ID'] == "SRP033494"
 
 
-def test4():
+def test4(rnaseq):
     try:
         import pandas
-        res = r.get_studies_by_organism("arabidopsis_thaliana", frmt='tsv')
+        res = rnaseq.get_studies_by_organism("arabidopsis_thaliana", frmt='tsv')
         studies = res['STUDY_ID'].values
     except:
-        res = r.get_studies_by_organism("arabidopsis_thaliana", frmt='tsv')
+        res = rnaseq.get_studies_by_organism("arabidopsis_thaliana", frmt='tsv')
         studies = [x[0] for x in res[1:]]
 
 
-def test5():
-    r.get_sample_attribute_per_run("SRR805786")
-    r.get_sample_attribute_per_run("SRR805786", frmt='tsv')
+def test5(rnaseq):
+    rnaseq.get_sample_attribute_per_run("SRR805786")
+    rnaseq.get_sample_attribute_per_run("SRR805786", frmt='tsv')
 
-    r.get_sample_attribute_per_study("SRP020492")
-    r.get_sample_attribute_per_study("SRP020492", frmt='tsv')
+    rnaseq.get_sample_attribute_per_study("SRP020492")
+    rnaseq.get_sample_attribute_per_study("SRP020492", frmt='tsv')
 
-    r.get_sample_attribute_per_study("SRP020492")
-    r.get_sample_attribute_per_study("SRP020492", frmt='tsv')
+    rnaseq.get_sample_attribute_per_study("SRP020492")
+    rnaseq.get_sample_attribute_per_study("SRP020492", frmt='tsv')
 
 
-def test_get_run():
-    res = r.get_run("SRR1042759")
+def test_get_run(rnaseq):
+    res = rnaseq.get_run("SRR1042759")
     assert res[0]['RUN_IDS'] == "SRR1042759"
