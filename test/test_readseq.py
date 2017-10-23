a8e70808d8aeab590f9a6156e050b00c82ed6405
Thomas Cokelaer <cokelaer@gmail.com>
2017-10-24 00:03:32 +0200

    update tests

--- test/test_readseq.py
+++ test/test_readseq.py
@@ -8,32 +8,17 @@ fasta = """MDAPRQVVNFGPGPAKLPHSVLLEIQKELLDYKGVGISVLEMSHRSSDFAKIINNTENLVRELLAVPDN
 
 def test_readseq():
 
-    # 8 is 
-    # 2 is GenBank
     s = readseq.Readseq()
-    jobid = s.run("cokelaer@test.co.uk", "test", sequence=fasta, inputformat=8, 
-            outputformat=2)
-    # should fail
-    genbank = s.get_result(s._jobid)
+    jobid = s.run(**{"email":"cokelaer@test.co.uk", "title":"test", "sequence":fasta,
+        "inputformat":"raw", "outputformat":"fasta", "stype":"protein"})
 
     import time
     count = 0
-    while s.get_status(s._jobid) != "FINISHED" and count<20:
+    while s.get_status(jobid) != "FINISHED" and count<20:
         count += 1
         time.sleep(1)
         print(count)
 
-    genbank = s.get_result(s._jobid)
-
-
-    # now let us do the inverse
-    jobid = s.run("cokelaer@test.co.uk", "test", sequence=genbank, inputformat=2, 
-            outputformat=8)
-    count = 0
-    while s.get_status(s._jobid) != "FINISHED" and count<20:
-        count += 1
-        time.sleep(1)
-        print(count)
     newfasta = s.get_result(s._jobid)
 
-    assert "DVQKLAAFMKKFLEMHQL" in newfasta
+    assert "DVQKLAAFMKKFLEMHQL" in newfasta.replace("\n", "")
