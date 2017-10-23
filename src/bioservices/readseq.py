56940fc5be4e758c7c795eaba1048572ca9b0c9a
Thomas Cokelaer <cokelaer@gmail.com>
2017-10-24 00:02:16 +0200

    replace readseq with seqret

--- src/bioservices/readseq.py
+++ src/bioservices/readseq.py
@@ -3,10 +3,10 @@
 #
 #  This file is part of bioservices software
 #
-#  Copyright (c) 2013-2014 - EBI-EMBL
+#  Copyright (c) 2013-2015 - EBI-EMBL
+#  Copyright (c) 2016-2017 - Institut Pasteur
 #
-#  File author(s):
-#      
+#  File author(s): thomas cokelaer
 #
 #  Distributed under the GPLv3 License.
 #  See accompanying file LICENSE.txt or copy at
@@ -16,81 +16,71 @@
 #  documentation: http://packages.python.org/bioservices
 #
 ##############################################################################
-#$Id$
-"""This module provides a class :class:`~Readseq` to access to Readseq WS.
+"""This module provides a class :class:`~Seqret` to access to Seqret WS.
 
 
-.. topic:: What is Readseq ?
+.. topic:: What is Seqret ?
 
-    :URL: http://www.ebi.ac.uk/Tools/sfc/readseq/
-    :Service: 
+    :URL: http://www.ebi.ac.uk/Tools/services/rest/seqret/
+    :Service:
     :Citations: http://www.ncbi.nlm.nih.gov/pubmed/18428689
 
     .. highlights::
 
-        Readseq reads and converts biosequences between a selection of common 
-        biological sequence formats, including EMBL, GenBank and fasta sequence 
-        formats. 
+        EMBOSS seqret reads and converts biosequences between a selection of common
+        biological sequence formats, including EMBL, GenBank and fasta sequence
+        formats.
 
-        Readseq homepage -- Sep 2014
-
-
-
-.. testsetup:: biomodels
-
-    from bioservices import Readseq
-    s = Readseq()
+        Seqret homepage -- Sep 2017
 
 
 """
 import copy
-from bioservices.services import WSDLService
+from bioservices.services import REST
 
 
-__all__ = ["Readseq"]
+__all__ = ["Seqret"]
 
 
 
-class Readseq(WSDLService):
-    """Interface to the `Readseq <http://www.ebi.ac.uk/readseq>`_ service
+class Seqret(REST):
+    """Interface to the `Seqret <http://www.ebi.ac.uk/readseq>`_ service
 
     ::
 
         >>> from bioservices import *
-        >>> s = Readseq()
+        >>> s = Seqret()
 
+    The ReadSeq service was replaced by the Seqret services (2015).
 
-    This services is retired from EBI (2015) and one should use seqret service
-    but the service works so let us keep it (not maintained though)
-
-    http://www.ebi.ac.uk/Tools/webservices/services/sfc/readseq_soap?rev=1425032285
-
-
+    .. versionchanged:: 0.15
 
     """
-    _url = "http://www.ebi.ac.uk/Tools/services/soap/readseq?wsdl"
+    _url = "https://www.ebi.ac.uk/Tools/services/rest/emboss_seqret"
     def __init__(self, verbose=True):
         """.. rubric:: Constructor
 
         :param bool verbose:
 
         """
-        super(Readseq, self).__init__(name="Readseq", url=Readseq._url, verbose=verbose)
+        super(Seqret, self).__init__(name="seqret", url=Seqret._url, verbose=verbose)
         self._parameters = None
 
     def _get_parameter(self):
         if self._parameters is None:
-            self._parameters = self.get_parameters()
+            self._parameters = self._get_parameters()
         return self._parameters
     parameters = property(_get_parameter, doc="Get list of parameter names")
 
-    def get_parameters(self):
+    def _get_parameters(self):
         """Get a list of the parameter names.
 
         :returns: a list of strings giving the names of the parameters.
 
         """
-        parameters = self.serv.getParameters().id
+        parameters = self.http_get("parameters", frmt="xml")
+        res = self.easyXML(parameters)
+        parameters = [x.text for x in res.findAll("id")]
         return parameters
 
     def get_parameter_details(self, parameter):
@@ -99,73 +89,95 @@ class Readseq(WSDLService):
         :param str parameter: identifier/name of the parameter to fetch details of.
         :return: a data structure describing the parameter and its values.
 
+        ::
+
+            rs = ReadSeq()
+            print(rs.get_parameter_details("stype"))
+
         """
-        self.devtools.check_param_in_list(parameter, self.parameters)
-        return self.serv.getParameterDetails(parameter)
+        parameters = self.http_get("parameterdetails/{}".format(parameter), frmt="xml")
+        res = self.easyXML(parameters)
+
+        results = []
+        for value in res.findAll('values')[0].findAll("value"):
+            items = value.findAll()
+            if len(items):
+                results.append({
+                    "text": items[0].text,
+                    "value": items[1].text,
+                    "defaultvalue": items[2].text})
+
+        return results
 
     def run(self, email, title, **kargs):
         """Submit a job to the service.
 
-        :param str email: user e-mail address. 
+        :param str email: user e-mail address.
         :param str title: job title.
         :param params: parameters for the tool as returned by :meth:`get_parameter_details`.
         :return: string containing the job identifier (jobId).
 
+        Deprecated (olf readseq service)::
 
+            Format Name     Value
+            Auto-detected   0
+            EMBL            4
+            GenBank         2
+            Fasta(Pearson)  8
+            Clustal/ALN     22
+            ACEDB           25
+            BLAST           20
+            DNAStrider      6
+            FlatFeat/FFF    23
+            GCG             5
+            GFF             24
+            IG/Stanford     1
+            MSF             15
+            NBRF            3
+            PAUP/NEXUS      17
+            Phylip(Phylip4)     12
+            Phylip3.2       11
+            PIR/CODATA      14
+            Plain/Raw       13
+            SCF             21
+            XML             19
 
-        Format Name     Value
-        Auto-detected   0
-        EMBL    4
-        GenBank     2
-        Fasta(Pearson)  8
-        Clustal/ALN     22
-        ACEDB   25
-        BLAST   20
-        DNAStrider  6
-        FlatFeat/FFF    23
-        GCG     5
-        GFF     24
-        IG/Stanford     1
-        MSF     15
-        NBRF    3
-        PAUP/NEXUS  17
-        Phylip(Phylip4)     12
-        Phylip3.2   11
-        PIR/CODATA  14
-        Plain/Raw   13
-        SCF     21
-        XML     19
-
-        As output, you also have 
+        As output, you also have
 
         Pretty 18
 
         ::
 
-            s = readseq.Readseq()
-            jobid = s.run("cokelaer@test.co.uk", "test", sequence=fasta, inputformat=8, 
+            s = readseq.Seqret()
+            jobid = s.run("cokelaer@test.co.uk", "test", sequence=fasta, inputformat=8,
                 outputformat=2)
             genbank = s.get_result(s._jobid)
-
-
-
-
-
 
 
         """
         for k in kargs.keys():
             self.devtools.check_param_in_list(k, self.parameters)
-        for k in ['inputformat','outputformat', 'outputcase', 'reverse', 'degap',
-                'feature', 'fthandle']:
+
+        assert "sequence" in kargs.keys()
+        params = {"email":email, "title":title}
+
+        for k in ['stype', 'inputformat','outputformat', "feature", "firstonly",
+                  "reverse", 'outputcase', 'seqrange']:
             if k in kargs.keys():
                 value = kargs.get(k)
 
-                valid_values = [x.value for x in self.get_parameter_details(k).values.value]
+                details = self.get_parameter_details(k)
+                valid_values = [x['value'] for x in details]
 
                 self.devtools.check_param_in_list(str(value), valid_values)
+                params[k] = value
+        #r = requests.post(url + "/run?", data={"sequence":fasta, "stype": "protein",
+        #"inputformat":"raw", "outputformat":"fasta", "email":"thomas.cokelaer@pasteur.fr",
+        #"title":"test"})
 
-        jobid = self.serv.run(email, title, kargs)
+        params['sequence'] = kargs['sequence']
+
+        jobid = self.http_post("run", frmt="txt", data=params)
         self._jobid = jobid
         return jobid
 
@@ -176,16 +188,16 @@ class Readseq(WSDLService):
         :return: string containing the status.
 
         The values for the status are:
-    
+
         - RUNNING: the job is currently being processed.
         - FINISHED: job has finished, and the results can then be retrieved.
         - ERROR: an error occurred attempting to get the job status.
         - FAILURE: the job failed.
         - NOT_FOUND: the job cannot be found.
 
-
         """
-        return self.serv.getStatus(jobid)
+        res = self.http_get("status/{}".format(jobid))
+        return res.content.decode()
 
     def get_result_types(self, jobid):
         """Get the available result types for a finished job.
@@ -193,24 +205,31 @@ class Readseq(WSDLService):
         :param str jobid: job identifier.
         :return: a list of wsResultType data structures describing the available result types.
         """
-        return self.serv.getResultTypes(jobid)
+        res = self.http_get("resulttypes/{}".format(jobid))
+
+        res = self.easyXML(res.content.decode())
+        this = res.findAll("types")[0].findAll("type")[0].findAll('identifier')
+        return this[0].text
 
     def get_result(self, jobid, parameters=None):
         """Get the result of a job of the specified type.
 
         :param str jobid: job identifier.
-        :param parameters: optional list of wsRawOutputParameter used to 
+        :param parameters: optional list of wsRawOutputParameter used to
             provide additional parameters for derived result types.
-        :return: the result data for the specified type, base64 encoded. 
-            Depending on the SOAP library and programming language used the 
-            result may be returned in decoded form. For some result types 
+        :return: the result data for the specified type, base64 encoded.
+            Depending on the SOAP library and programming language used the
+            result may be returned in decoded form. For some result types
             (e.g. images) this will be binary data rather than a text string.
         """
         if self.get_status(jobid) != 'FINISHED':
-            self.logging.warning("Your job is not finished yet. Try again.")
+            self.logging.warning("Your job is not finished yet. Try again later.")
             return
-        type_ = self.get_result_types(jobid).type[0].identifier
-        res = self.serv.getResult(jobid, type_)
+
+        result_types = self.get_result_types(jobid)
+
+        res = self.http_get("result/{}/{}".format(jobid, result_types))
+        res = res.content.decode()
 
         try:
             import base64
@@ -219,57 +238,3 @@ class Readseq(WSDLService):
             pass
         return res
 
-"""
-The input parameters for the job:
-
-Attribute   Type    Description
-inputformat     int     sequence format for input data
-outputformat    int     sequence format for output data
-outputcase  string  character case for output sequence data
-reverse     boolean     output reverse complement of input nucleotide sequence
-degap   string  remove gap symbols from sequence
-transymbol  string  replace specified base/residue symbol(s) in input sequence with specified base/residue symbol(s)
-feature     string  list of selected features to process
-fthandle    string  action to perform on selected features
-subrange    string  region of input sequence on which feature processing is performed
-sequence    string  input sequence data to process
-
-More detailed information about each parameter, including valid values can be obtained using the getParameterDetails(parameterId) operation.
-wsParameterDetails
-
-Descriptive information about a tool parameter. Returned by getParameterDetails(parameterId).
-Attribute   Type    Description
-name    string  Name of the parameter.
-description     string  Description of the parameter, suitable for use in option help interfaces.
-type    string  Data type of the parameter.
-values  list of wsParameterValue    Optional list of valid values for the option.
-wsParameterValue
-
-Description of a tool parameter value. Used in wsParameterDetails.
-Attribute   Type    Description
-label   string  Display name of the value, for use in interfaces.
-value   string  String representation of the value to be passed to the tool parameter.
-defaultValue    boolean     Flag indicating if this value is the default.
-properties  list of wsProperty  Optional list of key/value pairs providing further information.
-wsProperty
-
-Properties of a tool parameter value. Used in wsParameterValue.
-Attribute   Type    Description
-key     string  Property name.
-value   string  Property value.
-wsRawOutputParameter
-
-Additional parameters passed when requesting a result. See getResult(jobId, type, parameters).
-Attribute   Type    Description
-name    string  Parameter name.
-value   list of string  Parameter value.
-wsResultType
-
-Description of a result type. Returned by getResultTypes(jobId).
-Attribute   Type    Description
-identifier  string  Identifier for the result type. Passed as type to getResult(jobId, type, parameters).
-label   string  Display name for use in user interfaces.
-description     string  Description of the result type, for use in help interfaces.
-mediaType   string  MIME type of the returned data.
-fileSuffix  string  Suggested suffix for file name, if writing data to disk. 
-"""
