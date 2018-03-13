030ff797d9749e3ce943e6e5345f160f48fc25e4
Thomas Cokelaer <cokelaer@gmail.com>
2018-03-13 18:36:10 +0100

    Fixing reactome to the new API

--- src/bioservices/reactome.py
+++ src/bioservices/reactome.py
@@ -50,6 +50,451 @@ __all__ = ['Reactome', 'ReactomeAnalysis']
 
 
 class Reactome(REST):
+    """
+
+
+    pathway_diagram -> exporter_diagram
+
+    .. todo:: interactors, orthology, particiapnts, person,
+        query, refernces, schema
+
+    """
+
+    _url = "https://reactome.org/ContentService"
+    def __init__(self, verbose=True, cache=False):
+        super(Reactome, self).__init__("Reactome",url=Reactome._url,
+            verbose="ERROR", cache=False)
+        self.debugLevel = verbose
+
+    @property
+    def version(self):
+        return self.http_get("data/database/version")
+
+    def data_discover(self, identifier):
+        """The schema.org for an Event in Reactome knowledgebase
+
+        For each event (reaction or pathway) this method generates a
+        json file representing the dataset object as defined by
+        schema.org (http). This is mainly used by search engines in
+        order to index the data
+
+        ::
+
+            r.data_discover("R-HSA-446203")
+
+        """
+        res = self.http_get("data/discover/{}".format(identifier))
+        return res
+
+    def data_diseases(self):
+        """list of diseases objects"""
+        return self.http_get("data/diseases")
+
+    def data_diseases_doid(self):
+        """It retrieves the list of disease DOIDs annotated in Reactome
+
+        return: dictionary with DOID contained in the values()
+        """
+        res =  self.http_get("data/diseases/doid").content
+        res = dict([x.split() for x in res.decode().split("\n")])
+        return res
+
+    def exporter_diagram(self, identifier, ext="png", quality=5,
+        diagramProfile="Modern", analysisProfile="Standard", filename=None):
+        """Export a given pathway diagram to raster file
+
+        This method accepts identifiers for Event class instances.
+        When a diagrammed pathway is provided, the diagram is exported
+        to the specified format. When a subpathway is provided, the
+        diagram for the parent is exported and the events that are part
+        of the subpathways are selected. When a reaction is provided,
+        the diagram containing the reaction is exported and the reaction
+        is selected.
+
+        :param identifier: Event identifier (it can be a pathway with
+            diagram, a subpathway or a reaction)
+        :param ext: File extension (defines the image format) in png,
+            jpeg, jpg, svg, gif
+        :param quality: Result image quality between [1 - 10]. It
+            defines the quality of the final image (Default 5)
+        :param flg: not implemented
+        :param sel: not implemented
+        :param diagramProfile: Diagram Color Profile
+        :param token: not implemented
+        :param analysisProfile: Analysis Color Profile
+        :param expColumn: not implemented
+        :param filename: if given, save the results in the provided filename
+
+        return: raw data if filename parameter is not set. Otherwise, the data
+            is saved in the filename and the function returns None
+
+        """
+        assert ext in ['png', 'jpg', 'jpeg', 'svg', "gif"]
+        assert quality in range(11)
+        assert diagramProfile in ["Modern","Standard"]
+        assert analysisProfile in ["Standard", "Strosobar", "Copper Plus"]
+
+        params = {
+                    "diagramProfile": diagramProfile,
+                    "analysisProfile": analysisProfile,
+                    "quality": quality}
+
+        res = self.http_get("exporter/diagram/{}.{}".format(
+                identifier, ext), params=params)
+        if filename:
+            with open(filename, "wb") as fout:
+                import binascii
+                fout.write(res.content)
+        else:
+            return res
+
+    def data_complex_subunits(self, identifier, excludeStructuresSpecifies=False):
+        """A list with the entities contained in a given complex
+
+        Retrieves the list of subunits that constitute any given complex.
+        In case the complex comprises other complexes, this method
+        recursively traverses the content returning each contained
+        PhysicalEntity. Contained complexes and entity sets can be
+        excluded setting the ‘excludeStructures’ optional parameter to ‘true’
+
+        :param identifier: The complex for which subunits are requested
+        :param excludeStructures: Specifies whether contained complexes
+            and entity sets are excluded in the response
+
+        ::
+
+            r.data_complex_subunits("R-HSA-5674003")
+        """
+        params = {"excludeStructuresSpecifies": excludeStructuresSpecifies}
+        res = self.http_get("data/complex/{}/subunits".format(identifier),
+            params=params)
+        return res
+
+    def data_complexes(self, resources, identifier):
+        """A list of complexes containing the pair (identifier, resource)
+
+        Retrieves the list of complexes that contain a given (identifier,
+        resource). The method deconstructs the complexes into all its
+        participants to do so.
+
+        :param resource: The resource of the identifier for complexes are
+            requested (e.g. UniProt)
+        :param identifier: The identifier for which complexes are requested
+
+        ::
+
+            r.data_complexes(resources, identifier)
+            r.data_complexes("UniProt", "P43403")
+
+        """
+        res = self.http_get("data/complexes/{}/{}".format(
+                    resources, identifier))
+        return res
+
+    def data_entity_componentOf(self, identifier):
+        """A list of larger structures containing the entity
+
+        Retrieves the list of structures (Complexes and Sets) that
+        include the given entity as their component. It should be
+        mentioned that the list includes only simplified entries
+        (type, names, ids) and not full information about each item.
+
+        ::
+
+            r.data_entity_componentOf("R-HSA-199420")
+
+        """
+        res = self.http_get("data/entity/{}/componentOf".format(
+                    identifier))
+        return res
+
+    def data_entity_otherForms(self, identifier):
+        """All other forms of PhysicalEntity
+
+        Retrieves a list containing all other forms of the given
+        PhysicalEntity. These other forms are PhysicalEntities that
+        share the same ReferenceEntity identifier, e.g. PTEN
+        H93R[R-HSA-2318524] and PTEN C124R[R-HSA-2317439] are two
+        forms of PTEN.
+
+        ::
+
+            r.data_entity_otherForms("R-HSA-199420")
+
+        """
+        res = self.http_get("data/entity/{}/otherForms".format(
+                    identifier))
+        return res
+
+    def data_event_ancestors(self, identifier):
+        """The ancestors of a given event
+
+        The Reactome definition of events includes pathways and reactions.
+        Although events are organised in a hierarchical structure, a single
+        event can be in more than one location, i.e. a reaction can take
+        part in different pathways while, in the same way, a sub-pathway
+        can take part in many pathways. Therefore, this method retrieves
+        a list of all possible paths from the requested event to the top
+        level pathway(s).
+
+        :param identifier: The event for which the ancestors are requested
+
+        ::
+
+            r.data_event_ancestors("R-HSA-5673001")
+
+        """
+        res = self.http_get("data/event/{}/ancestors".format(identifier))
+        return res
+
+    def data_eventsHierarchy(self, species):
+        """The full event hierarchy for a given species
+
+        Events (pathways and reactions) in Reactome are organised in a
+        hierarchical structure for every species. By following all
+        ‘hasEvent’ relationships, this method retrieves the full event
+        hierarchy for any given species. The result is a list of tree
+        structures, one for each TopLevelPathway. Every event in these trees is
+        represented by a PathwayBrowserNode. The latter contains the stable identifier,
+        the name, the species, the url, the type, and the diagram of the particular
+        event.
+
+        :param species: Allowed species filter: SpeciesName (eg: Homo sapiens)
+            SpeciesTaxId (eg: 9606)
+
+        ::
+
+            r.data_eventsHierarchy(9606)
+        """
+
+        res = self.http_get("data/eventsHierarchy/{}".format(species))
+        return res
+
+    def exporter_sbml(self, identifier):
+        """Export given Pathway to SBML
+
+
+        :param identifier: DbId or StId of the requested database object
+
+        ::
+
+            r.exporter_sbml("R-HSA-68616")
+
+        """
+        res = self.http_get("exporter/sbml/{}.xml".format(identifier),
+                    frmt="xml")
+        return res
+
+
+    def data_pathway_containedEvents(self, identifier):
+        """All the events contained in the given event
+
+        Events are the building blocks used in Reactome to represent
+        all biological processes, and they include pathways and reactions.
+        Typically, an event can contain other events. For example, a
+        pathway can contain smaller pathways and reactions. This method
+        recursively retrieves all the events contained in any given event.
+
+        ::
+
+            res = r2.data_pathway_containedEvents("R-HSA-5673001")
+
+        """
+        res = self.http_get("data/pathway/{}/containedEvents".format(identifier))
+        return res
+
+    def data_pathway_containedEvents_by_attribute(self, identifier, attribute):
+        """A single property for each event contained in the given event
+
+        Events are the building blocks used in Reactome to represent all 
+        biological processes, and they include pathways and reactions. 
+        Typically, an event can contain other events. For example, a 
+        pathway can contain smaller pathways (subpathways) and reactions.
+        This method recursively retrieves a single attribute for each of 
+        the events contained in the given event.
+
+
+        :param identifier: The event for which the contained events are requested
+        :param attribute: Attrubute to be filtered
+
+        ::
+
+             r2.data_pathway_containedEvents_by_attribute("R-HSA-5673001", "stId")
+
+        """
+        res = self.http_get("data/pathway/{}/containedEvents/{}".format(
+            identifier, attribute))
+        return res.content
+ 
+    def data_pathways_low_diagram_entity(self, identifier):
+        """A list of lower level pathways with diagram containing 
+        a given entity or event
+
+        This method traverses the event hierarchy and retrieves the 
+        list of all lower level pathways that have a diagram and 
+        contain the given PhysicalEntity or Event.
+
+        :param identifier: The entity that has to be present in the pathways
+        :param species:  The species for which the pathways are requested. 
+            Taxonomy identifier (eg: 9606) or species name (eg: ‘Homo sapiens’)
+
+        ::
+
+            r.data_pathways_low_diagram_entity("R-HSA-199420")
+
+        """
+        res = self.http_get("data/pathways/low/diagram/entity/{}".format(identifier))
+        return res
+
+    def data_pathways_low_diagram_entity_allForms(self, identifier):
+        """
+
+        ::
+
+            r.data_pathways_low_diagram_entity_allForms("R-HSA-199420")
+        """
+        res = self.http_get("data/pathways/low/diagram/entity/{}/allForms".format(
+            identifier))
+        return res
+
+    def data_pathways_low_diagram_identifier_allForms(self, identifier):
+        """
+
+        ::
+
+            r.data_pathways_low_diagram_identifier_allForms("PTEN")
+
+        """
+        res = self.http_get("data/pathways/low/diagram/identifier/{}/allForms".format(identifier))
+        return res
+
+    def data_pathways_low_entity(self, identifier):
+        """A list of lower level pathways containing a given entity or event
+
+        This method traverses the event hierarchy and retrieves the 
+        list of all lower level pathways that contain the given     
+        PhysicalEntity or Event.
+
+        ::
+
+            r.data_pathways_low_entity("R-HSA-199420")
+        """
+        res = self.http_get("data/pathways/low/entity/{}".format(
+            identifier))
+        return res
+
+    def data_pathways_low_entity_allForms(self, identifier):
+        """A list of lower level pathways containing any form of a given entity 
+
+        This method traverses the event hierarchy and retrieves the list of all 
+        lower level pathways that contain the given PhysicalEntity in any of 
+        its variant forms. These variant forms include for example different 
+        post-translationally modified versions of a single protein, or the 
+        same chemical in different compartments.
+
+        ::
+
+            r.data_pathways_low_entity_allForms("R-HSA-199420")
+        """
+        res = self.http_get("data/pathways/low/entity/{}/allForms".format(
+            identifier))
+        return res
+
+    def data_pathways_top(self, species):
+        res = self.http_get("data/pathways/top/{}".format(species))
+        return res
+
+
+    def references(self, identifier):
+        """All referenceEntities for a given identifier
+
+        Retrieves a list containing all the reference entities for a given
+        identifier.
+
+        ::
+
+            r.references(15377)
+
+        """
+        res = self.http_get("references/mapping/{}".format(identifier))
+        return res
+
+    def search_facet(self):
+        """A list of facets corresponding to the whole Reactome search data
+
+        This method retrieves faceting information on the whole Reactome search data.
+
+
+        """
+        res = self.http_get("search/facet")
+        return res
+
+
+    def search_facet_query(self, query):
+        """A list of facets corresponding to a specific query
+
+        This method retrieves faceting information on a specific query
+
+        """
+        res = self.http_get("search/facet_query?query={}".format(query))
+        return res
+
+
+    def search_query(self, query):
+        """Queries Solr against the Reactome knowledgebase
+
+        This method performs a Solr query on the Reactome knowledgebase.
+        Results can be provided in a paginated format.
+
+        """
+        res = self.http_get("search/query?query={}".format(query))
+        return res
+
+    def search_spellcheck(self, query):
+        """Spell-check suggestions for a given query
+
+        This method retrieves a list of spell-check suggestions
+        for a given search term.
+
+        """
+        res = self.http_get("search/spellcheck?query={}".format(query))
+        return res
+
+    def search_suggest(self, query):
+        """Autosuggestions for a given query
+
+
+        This method retrieves a list of suggestions for a given search term.
+
+        ::
+
+            >>> r.http_get("search/suggest?query=apopt")
+            ['apoptosis', 'apoptosome', 'apoptosome-mediated', 'apoptotic']
+
+        """
+        res = self.http_get("search/suggest?query={}".format(identifier))
+        return res
+
+    def data_species_all(self):
+        """the list of all species in Reactome"""
+        res = self.http_get("data/species/all")
+        return res
+
+    def data_species_main(self):
+        """the list of main species in Reactome
+
+        ::
+
+            r = Reactome()
+            [x['displayName'] for x in r.data_species_main()]
+
+
+        """
+        res = self.http_get("data/species/main")
+        return res
+
+
+class ReactomeOld(REST):
     """Reactome interface
 
     some data can be download on the main `website <http://www.reactome.org/pages/download-data/>`_
@@ -92,7 +537,6 @@ class Reactome(REST):
         res = self._download_list_pathways()
         res = set([x[2] for x in self.get_list_pathways()])
         return res
-
 
     def biopax_exporter(self, identifier, level=2):
         """Get BioPAX file
@@ -151,10 +595,8 @@ class Reactome(REST):
             res = s.http_post("highlightPathwayDiagram/68875/PNG", frmt="txt",
                 data="CDC2")
             with open("test.png", 'wb') as f:
-                f.write(res.decode("base64"))
-                f.close()
-
-        .. todo:: Saving the image above returns a blank image ...
+                import binascii
+                f.write(binascii.a2b_base64(res))
 
 
         """
--- test/test_reactome.py
+++ test/test_reactome.py
@@ -1,25 +1,168 @@
 from bioservices import Reactome
 import pytest
+from easydev import TempFile
 
 
 @pytest.fixture
 def reactome():
     return Reactome(verbose=True)
 
+def test_data_discover(reactome):
+    res = reactome.data_discover("R-HSA-446203")
+    assert "name" in res
+
+def test_data_diseases(reactome):
+    res = reactome.data_diseases()
+    assert len(res)
+
+def test_data_diseases_doid(reactome):
+    res = reactome.data_diseases_doid()
+    assert len(res)
+
+def test_exporter_diagram(reactome):
+    res = reactome.exporter_diagram(109581, ext="png")
+    res = reactome.exporter_diagram(109581, ext="jpg")
+    res = reactome.exporter_diagram(109581, ext="jpg", quality=1)
+    res = reactome.exporter_diagram(109581, ext="jpg", quality=1,
+            diagramProfile="Standard")
+
+    with TempFile(suffix=".png") as fout:
+        res = reactome.exporter_diagram(109581, ext="png", filename=fout.name)
+
+def test_data_complex_subunits(reactome):
+    assert len(reactome.data_complex_subunits("R-HSA-5674003"))
+
+
+def test_data_complexes(reactome):
+
+    assert len(reactome.data_complexes("UniProt", "P43403"))
+    assert reactome.data_complexes("UniProt", "P434") == 404
+
+
+def test_data_entity_componentOf(reactome):
+    assert len(reactome.data_entity_componentOf("R-HSA-199420"))
+
+
+def test_data_entity_otherForms(reactome):
+    assert len(reactome.data_entity_otherForms("R-HSA-199420"))
+
+
+def test_data_event_ancestors(reactome):
+    assert len(reactome.data_event_ancestors("R-HSA-5673001"))
+
+def test_data_events_hierarchy(reactome):
+    assert len(reactome.data_eventsHierarchy(9606))
+
+
+def test_exporter_sbml(reactome):
+    assert len(reactome.exporter_sbml("R-HSA-68616"))
+
+
+def test_data_pathway_containedEvents(reactome):
+    res =  reactome.data_pathway_containedEvents("R-HSA-5673001")
+    assert len(res)
+
+
+def test_data_pathway_containedEvents_by_attribute(reactome):
+    res = reactome.data_pathway_containedEvents_by_attribute("R-HSA-5673001", "stId")
+    assert len(res)
+
+
+def test_data_pathways_low_diagram_entity(reactome):
+    res =  reactome.data_pathways_low_diagram_entity("R-HSA-199420")
+    assert len(res)
+
+
+def test_data_pathways_low_diagram_entity_allForms(reactome):
+    res =  reactome.data_pathways_low_diagram_entity_allForms("R-HSA-199420")
+    assert len(res)
+
+
+def test_data_pathways_low_diagram_identifier_allForms(reactome):
+    res =  reactome.data_pathways_low_diagram_identifier_allForms("PTEN")
+    assert len(res)
+
+
+def test_data_pathways_low_entity(reactome):
+    res =  reactome.data_pathways_low_entity("R-HSA-199420")
+    assert len(res)
+
+
+def test_data_pathways_low_entity_allForms(reactome):
+    res =  reactome.data_pathways_low_entity_allForms("R-HSA-199420")
+    assert len(res)
+
+
+def test_data_pathways_top(reactome):
+    res =  reactome.data_pathways_top(9606)
+    assert len(res)
+
+
+def test_references(reactome):
+    res = reactome.references(15377)
+    assert len(res)
+
+
+def test_search_facet(reactome):
+    res = reactome.search_facet()
+    assert len(res)
+
+
+def test_search_facet_query(reactome):
+    res = reactome.search_facet_query("R-HSA-199420")
+    assert len(res)
+
+
+def test_search_query(reactome):
+    res = reactome.search_query("R-HSA-199420")
+    assert len(res)
+
+
+def test_search_spellcheck(reactome):
+    res = reactome.search_spellcheck("appostosis")
+    assert len(res)
+
+
+def test_search_suggest(reactome):
+    res = reactome.search_spellcheck("apost")
+    assert len(res)
+
+
+def test_data_species_all(reactome):
+    res = reactome.data_species_all()
+    assert len(res)
+
+def test_data_species_main(reactome):
+    res = reactome.data_species_main()
+    assert len(res)
+
+
+"""
+
+@pytest.fixture
+def reactome():
+    return ReactomeOld(verbose=True)
+
+
 def test_species(reactome):
     assert len( reactome.get_species()) > 10
+
 
 def test_biopax_exporter(reactome):
     res = reactome.biopax_exporter(109581)
 
+
 def test_front_page_items(reactome):
     res = reactome.front_page_items("homo sapiens")
+
 
 def test_highlight_pathway_diagram(reactome):
     res = reactome.highlight_pathway_diagram(68875, frmt="PNG", genes="CDC2")
 
+
 def test_list_by_query(reactome):
     assert len(reactome.list_by_query("Pathway", name="Apoptosis"))>1
+
 
 def test_pathway_diagram(reactome):
     res = reactome.pathway_diagram(109581, 'XML')
@@ -31,38 +174,47 @@ def test_pathway_diagram(reactome):
     except:
         assert True
 
+
 def test_pathway_hierarchy(reactome):
     res = reactome.pathway_hierarchy('homo sapiens')
     assert len(res)>10
 
+
 def test_pathway_participants(reactome):
     res = reactome.pathway_participants(109581)
     assert len(res)>0
+
 
 def test_pathway_complexes(reactome):
     res = reactome.pathway_complexes(109581)
     assert len(res)>1
     assert "dbId" in list(res[0].keys())
 
+
 def test_query_by_id(reactome):
     res = reactome.query_by_id("Pathway", 109581)
     assert len(res)>1
+
 
 def test_query_by_ids(reactome):
     res = reactome.query_by_ids("Pathway", 'TP53')
     assert len(res)>=1
 
+
 def test_query_hit_pathways(reactome):
     assert len(reactome.query_hit_pathways("TP53"))>1
+
 
 def test_pathway_for_entities(reactome):
     res = reactome.query_pathway_for_entities("TP53")
 
+
 def test_species_list(reactome):
     res = reactome.species_list()
+
 
 def test_sbml_exporter(reactome):
     res = reactome.SBML_exporter(109581)
 
 
-
+"""
