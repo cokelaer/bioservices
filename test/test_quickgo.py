95c43a6e0a219cf7f143a81246f67052590a29d0
Thomas Cokelaer <cokelaer@gmail.com>
2018-06-14 15:20:34 +0200

    pep8

--- test/test_quickgo.py
+++ test/test_quickgo.py
@@ -5,19 +5,14 @@ import pytest
 def quickgo():
     return QuickGO(verbose=False, cache=False)
 
-
 def test_annotation_taxonid(quickgo):
     res = quickgo.Annotation(taxonId='9606')
-
 
 def test_annotation_aspect(quickgo):
     res = quickgo.Annotation(taxonId="9606", limit=100, aspect="F")
 
-
 def test_annotation_geneProductType(quickgo):
     res = quickgo.Annotation(taxonId="9606", limit=100, geneProductType="protein")
-
-
 
 def test_annotations_from_goid(quickgo):
     df = quickgo.Annotation_from_goid(goId="GO:0003824,GO:0003677", taxonId="9606" )
