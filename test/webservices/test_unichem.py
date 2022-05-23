0d887d050080a5b723807674de622178de946bbb
Thomas Cokelaer <cokelaer@gmail.com>
2022-05-23 11:15:19 +0200

    update unichem test to use new API

--- test/webservices/test_unichem.py
+++ test/webservices/test_unichem.py
@@ -6,79 +6,130 @@ def unichem():
     u = UniChem(verbose=False)
     return u
 
+# new API from v1.9 and unichem update in May 2022
 
-def test_get_compound_ids_from_src_id(unichem):
-    res1 = unichem.get_compound_ids_from_src_id("CHEMBL2", "chembl", "chebi")
-    res2 = unichem.get_compound_ids_from_src_id(["CHEMBL2"], "chembl", "chebi")
-    assert res1 == res2[0]
-    assert res1 == [{u'src_compound_id': u'8364'}]
-    assert res2[0] == [{u'src_compound_id': u'8364'}]
+def test_get_compounds(unichem):
+    res = unichem.get_compounds('GZUITABIAKMVPG-UHFFFAOYSA-N', 'inchikey')
+    res = res['compounds']
+    assert 'sources' in res[0]
+
+    res = unichem.get_compounds('CHEMBL12', 'chembl')
+    res = res['compounds']
+    assert 'sources' in res[0]
+
+    res = unichem.get_compounds('CHEMBL12', 1)
+    res = res['compounds']
+    assert 'sources' in res[0]
+    
+    assert unichem.get_compounds('CHEMBL12', 'dummy') == {}
+
+def test_get_sources_by_inchikey(unichem):
+
+    inchikey = "AAOVKJBEBIDNHE-UHFFFAOYSA-N"
+    res = unichem.get_sources_by_inchikey(inchikey)
+    assert unichem.get_sources_by_inchikey("AAOV") == {}
+    assert unichem.get_sources_by_inchikey(["AAOV"]) == {'AAOV': {}}
+
+def test_connectivity(unichem):
+    res = unichem.get_connectivity('GZUITABIAKMVPG-UHFFFAOYSA-N', 'inchikey')
+    assert 'sources' in res
+
+    res = unichem.get_connectivity('CHEMBL12', 'chembl')
+    assert 'sources' in res
+
+    res = unichem.get_connectivity('CHEMBL12', 1)
+    assert 'sources' in res
+
+    assert unichem.get_connectivity('CHEMBL12', 'dummy') == {}
+
+def test_images(unichem, tmpdir):
+
+    res = unichem.get_images('304698')
+    assert res
+
+    res = unichem.get_images('dummy')
+    assert res is None
+
+    outfile = tmpdir.join('test.svg')
+    res = unichem.get_images('304698', filename=outfile)
+
+
+def test_get_id_from_name(unichem):
+    res = unichem.get_id_from_name('chembl')
+    assert res == 1
+
+    assert unichem.get_id_from_name('dummmy') is None
+
+def test_get_sources(unichem):
+    unichem.get_sources()
 
 def test_get_all_src_ids(unichem):
-    assert len(unichem.get_all_src_ids())>=23
+    unichem.get_all_src_ids()
 
-def test_get_source_id(unichem):
-    assert unichem._get_source_id("chembl") == 1
-    assert unichem._get_source_id("1") == 1
-    assert unichem._get_source_id(1) == 1
+def test_get_source_info_by_name(unichem):
+    unichem.get_source_info_by_name('chembl')
+    assert unichem.get_source_info_by_name('du,,y') is None
 
-    try:
-        unichem._get_source_id("wrong")
-        assert False
-    except:
-        assert True
+def test_get_source_info_by_id(unichem):
+    unichem.get_source_info_by_id(1)
+    assert unichem.get_source_info_by_id(-1) is None
 
-    try:
-        unichem._get_source_id("20000")
-        assert False
-    except:
-        assert True
+def test_get_inchi_from_inchikey(unichem):
+    res = unichem.get_inchi_from_inchikey("AAOVKJBEBIDNHE-UHFFFAOYSA-N")
+    assert 'standardinchi' in res[0]
 
-def test_get_src_information(unichem):
-    assert unichem.get_source_information("chebi")['name'] == "chebi"
+    inchikey1 = "AAOVKJBEBIDNHE-UHFFFAOYSA-N"
+    inchikey2 = "GZUITABIAKMVPG-UHFFFAOYSA-N"
+    res = unichem.get_inchi_from_inchikey([inchikey1,inchikey2])
+    assert 'standardinchi' in res[inchikey1][0]
+    assert 'standardinchi' in res[inchikey2][0]
 
-    assert unichem.get_source_information(['chembl', 'drugbank'])[0]['name']=="chembl"
+def test_get_sources_by_inchikey(unichem):
 
-def test_get_all_compound_ids_from_src_id(unichem):
-    res = unichem.get_all_compound_ids_from_all_src_id("CHEMBL12", "chembl")
-    res = unichem.get_all_compound_ids_from_all_src_id("CHEMBL12", "chembl", "chebi")
+    # assume everything is alright:
+    inchikey1 = "AAOVKJBEBIDNHE-UHFFFAOYSA-N"
+    inchikey2 = "GZUITABIAKMVPG-UHFFFAOYSA-N"
 
-def test_mapping(unichem):
-    res = unichem.get_mapping("kegg_ligand", "chembl")
-    assert len(res)>0
+    # uses only one string as input
+    res = unichem.get_sources_by_inchikey(inchikey1)
+    assert 'src_compound_id' in res[0]
 
-def test_get_src_compound_ids_from_inchikey(unichem):
-    unichem.get_src_compound_ids_from_inchikey("AAOVKJBEBIDNHE-UHFFFAOYSA-N")
-    unichem.get_src_compound_ids_from_inchikey(["AAOVKJBEBIDNHE-UHFFFAOYSA-N"])
-    #unichem.get_src_compound_ids_all_from_inchikey("AAOVKJBEBIDNHE-UHFFFAOYSA-N")
-    #unichem.get_src_compound_ids_all_from_inchikey(["AAOVKJBEBIDNHE-UHFFFAOYSA-N"])
+    # uses a list
+    res = unichem.get_sources_by_inchikey([inchikey1, inchikey2])
+    assert len(res) == 2
+    assert 'src_compound_id' in res[inchikey2][0]
+    assert 'src_compound_id' in res[inchikey1][0]
 
-def test_structure(unichem):
-    unichem.get_structure("CHEMBL12", "chembl")
-    unichem.get_structure(["CHEMBL12"], "chembl")
-    unichem.get_structure_all("CHEMBL12", "chembl")
+    # Now let us make a mistake:
+    res = unichem.get_sources_by_inchikey("dummy")
+    assert res == {}
+    res = unichem.get_sources_by_inchikey(["dummy1", 'dummy2'])
+    assert res['dummy1'] == {}
+    assert res['dummy2'] == {}
 
-def test_get_src_compound_id_url(unichem):
-    unichem.get_src_compound_id_url("CHEMBL12", "chembl", "drugbank")
-    unichem.get_src_compound_id_url(["CHEMBL12"], "chembl", "drugbank")
+def test_get_sources_by_inchikey_verbose(unichem):
 
-def test_get_src_compound_ids_all_from_obsolete(unichem):
-    unichem.get_src_compound_ids_all_from_obsolete("DB07699", "2")[0]
-    unichem.get_src_compound_ids_all_from_obsolete("DB07699", "2", "2")[0]
+    # assume everything is alright:
+    inchikey1 = "AAOVKJBEBIDNHE-UHFFFAOYSA-N"
+    inchikey2 = "GZUITABIAKMVPG-UHFFFAOYSA-N"
 
-#SLOW one
-def test_get_verbose_src_compound_ids_from_inchikey(unichem):
-    assert unichem.get_verbose_src_compound_ids_from_inchikey("GZUITABIAKMVPG-UHFFFAOYSA-N") != 400
-    assert unichem.get_verbose_src_compound_ids_from_inchikey("QFFGVLORLPOAEC-SNVBAGLBSA-N") != 400 
+    # uses only one string as input
+    res = unichem.get_sources_by_inchikey_verbose(inchikey1)
+    assert 'src_compound_id' in res[0]
+
+    # uses a list
+    res = unichem.get_sources_by_inchikey_verbose([inchikey1, inchikey2])
+    assert len(res) == 2
+    assert 'src_compound_id' in res[inchikey2][0]
+    assert 'src_compound_id' in res[inchikey1][0]
+
+    # Now let us make a mistake:
+    res = unichem.get_sources_by_inchikey_verbose("dummy")
+    assert res == {}
+    res = unichem.get_sources_by_inchikey_verbose(["dummy1", 'dummy2'])
+    assert res['dummy1'] == {}
+    assert res['dummy2'] == {}
 
 
-@pytest.mark.xfail
-def test_get_auxiliary_mapping(unichem):
-    # useless since we have a global timeout for pytest of 10 o r30 seconds
-    unichem.settings.TIMEOUT = 100
-    res = unichem.get_auxiliary_mappings(1)
-    if isinstance(res, int):
-        pass
-    else:
-        assert len(res)>0
-
+def test_get_structure(unichem):
+    assert unichem.get_structure("CHEMBL12", "chembl")
